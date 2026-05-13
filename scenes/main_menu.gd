3c10d2cb7f06f4c9c48022d22aafdca80bd406b0
Pedro Valente <pedrohenriquepvalente@gmail.com>
2026-05-13 09:39:23 -0300

    menu

---
+++ scenes/main_menu.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$CanvasLayer/Control/VBoxContainer/PlayButton.pressed.connect(_on_play_pressed)
+	$CanvasLayer/Control/VBoxContainer/QuitButton.pressed.connect(_on_quit_pressed)
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/map1.tscn")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
