b823c7961c0c225ab9ebb025db1714c028bf4552
Pedro Valente <pedrohenriquepvalente@gmail.com>
2026-05-19 08:52:35 -0300

    som add

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
+@onready var som_botao: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$CanvasLayer/Control/VBoxContainer/PlayButton.pressed.connect(_on_play_pressed)
 	$CanvasLayer/Control/VBoxContainer/QuitButton.pressed.connect(_on_quit_pressed)
 
 func _on_play_pressed() -> void:
+	som_botao.play()
+	await som_botao.finished
+	await get_tree().create_timer(0.1).timeout
 	get_tree().change_scene_to_file("res://scenes/choose_map.tscn")
 
 func _on_quit_pressed() -> void:
