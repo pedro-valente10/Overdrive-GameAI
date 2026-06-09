2d79e402eee68cad7b9b1f9e4fb2cee1eda1d513
Pedro Valente <pedrohenriquepvalente@gmail.com>
2026-06-09 09:05:37 -0300

    ups

--- scenes/car1button.gd
+++
@@ -1,7 +0,0 @@
-extends Button
-
-func _ready() -> void:
-	self.pressed.connect(_on_car1_pressed)
-
-func _on_car1_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/map1.tscn")
---
+++ scenes/choose_car.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+
+func _ready() -> void:
+	$CanvasLayer/Control/VBoxContainer/Car1Button.pressed.connect(_on_map1_pressed)
+
+func _on_map1_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/map1.tscn")
