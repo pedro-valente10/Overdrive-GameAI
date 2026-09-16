9be1eda88437492cccee119b5c6aebd959f17e32
Pedro Valente <pedrohenriquepvalente@gmail.com>
2026-09-16 14:49:27 -0300

    up

--- scenes/options.gd
+++ scenes/options.gd
@@ -5,7 +5,3 @@ func _ready() -> void:
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
